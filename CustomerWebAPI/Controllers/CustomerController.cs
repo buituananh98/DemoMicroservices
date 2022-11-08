7f6122273ca6c265c63742c49629f8782a63167c
Anh Bui Tuan <btanh1@cmcglobal.vn>
2022-11-08 15:46:06 +0700

    jkj

--- CustomerWebAPI/Controllers/CustomerController.cs
+++ CustomerWebAPI/Controllers/CustomerController.cs
@@ -88,7 +88,7 @@ namespace CustomerServices.Controllers
                 var customer = await _customerDbContext.Customers.FindAsync(customerId);
                 if (customer != null)
                 {
-                    //design partten
+                    //design partten 
                     _customerDbContext.Customers.Remove(customer);
                     await _customerDbContext.SaveChangesAsync();
                     return Ok();
